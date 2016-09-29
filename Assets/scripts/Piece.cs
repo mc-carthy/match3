09a9c8f48e120c6a9d3998f7509aef5b898245fe
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-29 15:20:58 +0100

    Add Piece class and randomly choose from available colours upon instantiation

---
+++ Assets/scripts/Piece.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour {
+
+	private Renderer ren;
+	private Color[] colors = new Color[6] {
+		Color.red,
+		Color.blue,
+		Color.green,
+		Color.white,
+		Color.yellow,
+		Color.magenta
+	};
+	private int index;
+	private Vector2 coords;
+	private bool isDestroyed;
+
+	private void Start () {
+		index = Random.Range (0, colors.Length);
+		ren = GetComponent<Renderer> ();
+		ren.material.SetColor ("_Color", colors [index]);
+	}
+}
